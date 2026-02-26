f7017e48071f3c2cd026d25eb29c451fdd7a0639
nazarifard <bahador.nazarifard@gmail.com>
2026-02-26 19:01:31 +0330

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,2 +1,7 @@
 <?php
-// entry point for integration_karban
+declare(strict_types=1);
+
+namespace OCA\Karban\AppInfo;
+
+// This file is used to initialize the app.
+// Logic will be added in future versions.
